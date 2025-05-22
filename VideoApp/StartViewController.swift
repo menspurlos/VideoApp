17316496dd56ee360ee8d4637a80b12b5c828016
Dmitry Grishkin <grishkin.dv@dns-shop.ru>
2025-05-22 10:29:34 +0700

    remove story board

--- VideoApp/StartViewController.swift
+++ VideoApp/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  VideoApp
 //
 //  Created by Дима Гришкин on 22.05.2025.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
